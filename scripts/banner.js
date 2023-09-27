1d98ed49d2cd5be64b2110046cb8e23d9d5c1186
liuyuchuan <18611448841@163.com>
2023-09-27 18:04:04 +0800

    调通版

---
+++ scripts/banner.js
@@ -0,0 +1,8 @@
+import { version } from '../package.json';
+
+export default `/**!
+* sortable ${ version }
+* @author RubXa <trash@rubaxa.org>
+* author owenm <owen23355@gmail.com>
+* @license MIT
+*/`
